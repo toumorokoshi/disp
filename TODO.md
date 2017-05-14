1c46e9f50fbfa8cac2ea9552f0a1740d7e303853
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2017-05-13 20:22:53 -0700

    adding while_production check.

--- TODO.md
+++ TODO.md
@@ -1,2 +1,4 @@
 * list should execute all statements
 * modify if statement to take a statement-list as next arg
+* implement while
+* implement match
--- src/codegen/builtins.rs
+++ src/codegen/builtins.rs
@@ -72,6 +72,20 @@ pub fn if_production(context: &mut Context, args: &[Token]) -> CodegenResult {
     return Ok(Object{typ: ghvm::Type::Int, register: return_value.register});
 }
 
+pub fn while_production(context: &mut Context, args: &[Token]) -> CodegenResult {
+    let start_index = context.builder.ops.len();
+    let return_value = context.builder.allocate_local(&ghvm::Type::Int);
+    let condition = try!(ensure_type!(ghvm::Type::Bool, try!(gen_token(context, &args[0]))));
+    // placeholder for the condition check
+    context.builder.ops.push(ghvm::Op::Noop{});
+    let branch_index = context.builder.ops.len();
+    let return_value = try!(gen_token(context, &args[1]));
+    context.builder.ops.push(ghvm::Op::Goto{position: start_index});
+    let loop_end_index = context.builder.ops.len();
+    context.builder.ops[branch_index] = ghvm::Op::Branch{condition: condition.register, if_false: context.builder.ops.len()};
+    return Ok(Object{typ: ghvm::Type::Int, register: return_value.register});
+}
+
 
 pub fn print(vm: &mut ghvm::VM, mut args: ghvm::ValueList) -> ghvm::Value {
     println!("{0}", args[0]);
