4bc2194029dff9b2fab926fb7925cc4a1f5e97aa
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2018-06-24 20:38:08 -0700

    Added thread pinning of workers

--- docs/architecture.md
+++ docs/architecture.md
@@ -7,4 +7,11 @@ warpspeed exposes this via the concept of workers.
 
 At the start, and by default, warpspeed creates N worker threads, one per CPU (or hyperthread). These worker threads will then be proceeded to be pinned to each available CPU, if possible.
 
-By pinning a worker to a CPU, it ensures that one can take advantage of cache locality. By specifying a specific CPU on which a fiber should run, it also better ensures cache locality.
+By pinning a worker to a CPU, it ensures that one can take advantage of cache locality. By specifying a specific worker on which a fiber should run, it also better ensures cache locality between it and any spawned fibers.
+
+## Fiber Efficiency
+
+Fibers maximize cache affinity by ensuring spawned fibers also run
+on the same worker. By virtue of a worker only running a single fiber at a given
+moment in time, fibers can also eliminate a majority of the locking that is commonly
+required to ensure thread (in this case fiber) safety of operations.
--- src/runtime/pool.rs
+++ src/runtime/pool.rs
@@ -1,4 +1,8 @@
 use num_cpus;
+use nix::{
+    sched::{sched_setaffinity, CpuSet},
+    unistd::Pid,
+};
 use std::{
     thread::{spawn, JoinHandle},
     sync::mpsc::channel,
@@ -21,9 +25,10 @@ impl WorkerPool {
     pub fn new() -> WorkerPool {
         let num = num_cpus::get();
         let mut workers = Vec::with_capacity(num);
-        for _ in 0..num {
+        for i in 0..num {
             let (tx, rx) = channel();
             let thread_handle = spawn(move || {
+                set_affinity(i);
                 let mut runtime = Runtime::new().unwrap();
                 {
                     tx.send(runtime.handle()).unwrap();
@@ -44,4 +49,14 @@ impl WorkerPool {
     pub fn len(&self) -> usize {
         return self.workers.len();
     }
+}
+
+/// set the thread in question to run on the cpu specified,
+/// preferably.
+fn set_affinity(cpu_num: usize) {
+    let mut cpu_set = CpuSet::new();
+    cpu_set.set(cpu_num).unwrap();
+    // setting affinity from 0 will set it for the current
+    // thread
+    sched_setaffinity(Pid::from_raw(0), &cpu_set).unwrap();
 }
