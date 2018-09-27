ebc1b513a051be8b4a1742dc0e23f4d156e8aeb9
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2018-09-26 23:19:17 -0700

    stopgap.

--- src/llvm_builder/mod.rs
+++ src/llvm_builder/mod.rs
@@ -1,5 +1,61 @@
-use llvm_sys;
-use std::ptr;
+use llvm_sys::{
+    self,
+    core,
+    execution_engine,
+    target,
+    prelude::{
+        LLVMBuilderRef,
+        LLVMContextRef,
+        LLVMModuleRef,
+    }
+};
+use std::{
+    mem,
+    ptr
+};
+
+pub struct LLVMBuilder {
+    context: LLVMContextRef,
+    module: LLVMModuleRef,
+    builder: LLVMBuilderRef,
+}
+
+impl LLVMBuilder {
+    pub fn new() -> LLVMBuilder {
+        unsafe {
+            let context = core::LLVMContextCreate();
+            let module = core::LLVMModuleCreateWithNameInContext(b"main\0".as_ptr() as *const _, context);
+            let builder = core::LLVMCreateBuilderInContext(context);
+            return LLVMBuilder {
+                context: context,
+                module: module,
+                builder: builder
+            };
+        }
+    }
+
+    /// build the body of the main function, for now.
+    pub fn build_function() {
+    }
+
+    /// run the module. In the future, this will
+    /// be replaced by simply compiling and writing out an executable.
+    /// Although, this may remain as we need to run code during compile time.
+    pub fn run(&self) {
+        unsafe {
+            let mut ee = mem::uninitialized();
+            let mut out = mem::zeroed();
+            execution_engine::LLVMLinkInMCJIT();
+            target::LLVM_InitializeNativeTarget();
+            target::LLVM_InitializeNativeAsmPrinter();
+            execution_engine::LLVMCreateExecutionEngineForModule(&mut ee, self.module, &mut out);
+            let addr = execution_engine::LLVMGetFunctionAddress(ee, b"main\0".as_ptr() as *const _);
+            let f: extern "C" fn() = mem::transmute(addr);
+            f();
+            execution_engine::LLVMDisposeExecutionEngine(ee);
+        }
+    }
+}
 
 pub fn build_function() {
     unsafe {
