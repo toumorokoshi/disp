1333559eef5f679778865c19f0d2ab8ab3f07e14
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2018-10-23 23:09:29 -0700

    continuing to flesh out heeren implementation

--- inference/src/heeren.rs
+++ inference/src/heeren.rs
@@ -5,16 +5,22 @@ use std::rc::Rc;
 pub fn solve_types(assumptions: &AssumptionSet) -> Result<SubstitutionSet, String> {
 }
 
+
+macro_rules! upsert {
+    ($x:ident, $y:expr) => {
+    }
+}
+
 /// AssumptionSets store the assumptions made around
 /// specific type variables.
-pub struct AssumptionSet {
-    pub constraints: Vec<Constraint>,
+pub struct AssumptionSet<T> {
+    pub constraints: Vec<Constraint<T>>,
     /// all the types that have been instantiated for the assumption set.
     pub types: Vec<Rc<TypeVar>>,
 }
 
-impl AssumptionSet {
-    pub fn new() -> AssumptionSet {
+impl AssumptionSet<T> {
+    pub fn new() -> AssumptionSet<T> {
         AssumptionSet {
             constraints: vec![],
             types: vec![],
@@ -28,21 +34,36 @@ impl AssumptionSet {
         var.clone()
     }
 
-    pub fn add_constraint(&mut self, constraint: Constraint) {
-    }
+    /// return a substitution set that satisfies the 
+    /// constraints provided.
+    /// 
+    /// this method can fail if there are contradicting constraints,
+    /// or if there are an insufficient amount of constraints to make a 
+    /// concrete decision
+    pub solve(&self) -> SubstitutionSet<T> {
+        let result_set = SubstitutionSet::new();
+        let mut constraint_by_typevar = HashMap::new();
+        // first, we run through all constraints, and reorganize so that
+        // we order constraints by the typevar we are operating on.
+        for ref c in &self.constraints {
+            match c {
+                Constraint::Equality(ref lhs, ref rhs) => {
+                }
+            }
+        }
+   }
 }
 
 /// stores the final result of the type inference algorithm.
-pub struct SubstitutionSet(HashMap<Rc<TypeVar>>, TypeLiteral);
-
-pub type TypeLiteral = String;
+pub struct SubstitutionSet<T>(HashMap<Rc<TypeVar>>, Option<T>);
 
 /// A TypeVar collects assumptions around this variable
 pub type TypeVar usize;
 
 /// Constraints help deduce the actual type of a type variables.
-/// there are a few types.
-pub enum Constraint {
+/// there are a few types.i
+#[derive(Clone)]
+pub enum Constraint<T> {
     /// specifie that the types introduced must be
     /// equal to each other.
     Equality(Rc<TypeVar>, Rc<TypeVar>),
@@ -51,12 +72,13 @@ pub enum Constraint {
     // specifies that the LHS should be the result of generalization the RHS.
     ImplicitInstanceConstraint(Rc<TypeVar>, Rc<TypeVar>),
     /// declares that the type of typevar is of the literal.
-    IsLiteral(Rc<TypeVar>, TypeLiteral),
-    }
+    IsLiteral(Rc<TypeVar>, T),
+}
 
 
 pub enum Expression {
     Literal(TypeLiteral),
+    App(Box<Expression>, Box<Expression>),
     // TODO: seems like there needs to be a way to express a list of types,
     // otherwise it's not possible to define something like a function signature.
 }
--- inference/src/heeren_tests.rs
+++ inference/src/heeren_tests.rs
@@ -1,11 +1,31 @@
 use super::{AssumptionSet, Constraint,SubstitutionSet};
 
+enum ExamplesTypes {
+    Bool,
+    FunctionType(Vec<Box<ExampleTypes>>, Box<ExamplesTypes>),
+    TypeVariable(Rc<TypeVar>),
+}
+
 #[test]
 fn test_example_from_paper() {
-    let mut assumptions = AssumptionSet::new();
-    let m = assumptions.create_type_var();
-    let y = assumptions.create_type_var();
-    let y_result = assumptions.create_type_var();
-    assumptions.add_constraint(Constraint::App
-    assumptions.
+    let mut assumptions = AssumptionSet<ExamplesTypes>::new();
+    let t1 = assumptions.create_type_var();
+    let t2 = assumptions.create_type_var();
+    let t3 = assumptions.create_type_var();
+    let t4 = assumptions.create_type_var();
+    let t5 = assumptions.create_type_var();
+    /// ad constraint that y must consume a bool, and that
+    ///
+    assumptions.constraints.extend(vec![
+        Constraint::IsLiteral(t2.clone(), 
+        ExamplesTypes::FunctionType(
+            vec![ExampleTypes::Bool],
+            ExampleTypes::TypeVariable(t3.clone()),
+        ]),
+        Constraint::ImplicitInstanceConstraint(t4.clone(), t5.clone(), t3.clone()),
+        Constraint::ImplicitInstanceConstraint(t2.clone(), t5.clone(), t1.clone()),
+        Constraint::Equality(t5.clone(), t1.clone()),
+    ]);
+    let substitution = assumptions.solve();
+    assert!(substitution.get);
 }
