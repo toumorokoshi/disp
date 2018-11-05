81839b736ee44fd23c30c06bd25c499bc28be5ac
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2018-11-04 21:11:57 -0800

    starting on unification

--- inference/src/heeren.rs
+++ inference/src/heeren.rs
@@ -1,14 +1,20 @@
+use std::collections::HashMap;
 /// An implementation of Heeren's algorithm for type inference.
 use std::rc::Rc;
 
 /// given an assumption set of assumptions, return back a valid substitution set if possible.
-pub fn solve_types(assumptions: &AssumptionSet) -> Result<SubstitutionSet, String> {
+pub fn solve_types<T>(assumptions: &AssumptionSet<T>) -> Result<SubstitutionSet<T>, String> {
+    panic!();
 }
 
-
 macro_rules! upsert {
-    ($x:ident, $y:expr) => {
-    }
+    ($set: ident, $x:ident, $y:expr) => {
+        if let Some(value) = $set.get_mut($x) {
+            value.push($y);
+        } else {
+            $set.insert($x.clone(), vec![$y]);
+        }
+    };
 }
 
 /// AssumptionSets store the assumptions made around
@@ -19,7 +25,7 @@ pub struct AssumptionSet<T> {
     pub types: Vec<Rc<TypeVar>>,
 }
 
-impl AssumptionSet<T> {
+impl<T> AssumptionSet<T> {
     pub fn new() -> AssumptionSet<T> {
         AssumptionSet {
             constraints: vec![],
@@ -40,25 +46,50 @@ impl AssumptionSet<T> {
     /// this method can fail if there are contradicting constraints,
     /// or if there are an insufficient amount of constraints to make a
     /// concrete decision
-    pub solve(&self) -> SubstitutionSet<T> {
-        let result_set = SubstitutionSet::new();
-        let mut constraint_by_typevar = HashMap::new();
+    pub fn solve(&self) -> SubstitutionSet<T> {
+        let mut result_set = SubstitutionSet::new();
+        let mut constraint_by_typevar: HashMap<Rc<TypeVar>, Vec<Constraint<T>>> = HashMap::new();
         // first, we run through all constraints, and reorganize so that
         // we order constraints by the typevar we are operating on.
         for ref c in &self.constraints {
             match c {
                 Constraint::Equality(ref lhs, ref rhs) => {
-                }
+                    upsert!(constraint_by_typevar, lhs, *c.clone());
+                    upsert!(constraint_by_typevar, rhs, *c.clone());
+                },
+                Constraint::IsLiteral(ref lhs, ref type) => {
+                    upsert!(constraint_by_typevar, lhs, *c.clone());
+                },
+                Constraint::IsGeneric(ref lhs, ref rhs) => {
+                    upsert!(constraint_by_typevar, lhs, *c.clone());
+                },
+                Constraint::ImplicitInstanceConstraint(ref lhs, ref rhs) => {
+                    upsert!(constraint_by_typevar, lhs, *c.clone());
+                },
             }
         }
-   }
+        // once it is organized, we star by solving one variable at a time
+        result_set
+    }
+
+    /// solve for one specific type. Solve for others if need be.
+    fn solve_one_type(constraint_by_typevar: &HashMap<Rc<TypeVar>, Vec<Constraint<T>>>,
+                      substitution_set: &mut SubstitutionSet,
+                      target: Rc<TypeVar>) {
+    )
 }
 
 /// stores the final result of the type inference algorithm.
-pub struct SubstitutionSet<T>(HashMap<Rc<TypeVar>>, Option<T>);
+pub struct SubstitutionSet<T>(HashMap<Rc<TypeVar>, Option<T>>);
+
+impl<T> SubstitutionSet<T> {
+    pub fn new() -> SubstitutionSet<T> {
+        SubstitutionSet(HashMap::new())
+    }
+}
 
 /// A TypeVar collects assumptions around this variable
-pub type TypeVar usize;
+pub type TypeVar = usize;
 
 /// Constraints help deduce the actual type of a type variables.
 /// there are a few types.i
@@ -73,12 +104,4 @@ pub enum Constraint<T> {
     ImplicitInstanceConstraint(Rc<TypeVar>, Rc<TypeVar>),
     /// declares that the type of typevar is of the literal.
     IsLiteral(Rc<TypeVar>, T),
-}
-
-
-pub enum Expression {
-    Literal(TypeLiteral),
-    App(Box<Expression>, Box<Expression>),
-    // TODO: seems like there needs to be a way to express a list of types,
-    // otherwise it's not possible to define something like a function signature.
 }
--- inference/src/lib.rs
+++ inference/src/lib.rs
@@ -4,32 +4,29 @@
 use std::rc::Rc;
 mod heeren;
 
-
-
-pub fn infer_type() {
-}
+pub fn infer_type() {}
 
 /// Type Inference algorithms work by defining expressions, which effectively
 /// define constraints on what the types themselves can be.
 /// This enum defines the available constraints,
 /// using the naming standardized by Hindley-Milner.
 pub enum Expression {
-    /// Var declares a new variable that a type variable may resolve to.
-    Var(Type),
-    /// App declares the result
-    App(Expression, Expression)
-    /// Abs declares that for some input Type, there
-    /// exists a transformation of that type to the right-hand-side
-    /// expression.
-    Abs(Type, Expression),
-    Equals(Rc<Type>, Rc<Type>),
-    IsGeneric(Rc<Type>, Rc<Type>),
+    // /// Var declares a new variable that a type variable may resolve to.
+// Var(Type),
+// /// App declares the result
+// App(Box<Expression>, Box<Expression>),
+// /// Abs declares that for some input Type, there
+// /// exists a transformation of that type to the right-hand-side
+// /// expression.
+// Abs(Type, Expression),
+// Equals(Rc<Type>, Rc<Type>),
+// IsGeneric(Rc<Type>, Rc<Type>),
 }
-/// The TypeEnvironment maps type references to their
-/// real type values
-pub struct TypeEnvironment {
-    mapping: HashMap<Type, PolyType>;
-}
+// /// The TypeEnvironment maps type references to their
+// /// real type values
+// pub struct TypeEnvironment {
+//     mapping: HashMap<Type, PolyType>,
+// }
 
 /// TODO: make this more generic, potentially allowing
 /// consumption of types.
