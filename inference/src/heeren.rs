812f47a85cc64ddefc0f5365713c52107c18c1c5
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2018-11-23 22:54:49 -0800

    have a unit test passing for basic unification

--- inference/src/heeren.rs
+++ inference/src/heeren.rs
@@ -1,12 +1,15 @@
 use std::collections::HashMap;
 /// An implementation of Heeren's algorithm for type inference.
+use std::fmt::Debug;
 use std::rc::Rc;
-
 
 /// Constraints help deduce the actual type of a type variables.
 /// there are a few types.i
-#[derive(Clone)]
-pub enum Constraint<T> {
+#[derive(Clone, PartialEq)]
+pub enum Constraint<T>
+where
+    T: Clone + PartialEq + Debug,
+{
     /// specifie that the types introduced must be
     /// equal to each other.
     Equality(Rc<TypeVar>, Rc<TypeVar>),
@@ -18,13 +21,15 @@ pub enum Constraint<T> {
     IsLiteral(Rc<TypeVar>, T),
 }
 
-
 /// A TypeVar collects assumptions around this variable
 pub type TypeVar = usize;
 
 /// AssumptionSets store the assumptions made around
 /// specific type variables.
-pub struct ConstraintSet<T> {
+pub struct ConstraintSet<T>
+where
+    T: Clone + PartialEq + Debug,
+{
     pub constraints: Vec<Constraint<T>>,
     /// all the types that have been instantiated for the assumption set.
     pub types: Vec<Rc<TypeVar>>,
@@ -32,56 +37,132 @@ pub struct ConstraintSet<T> {
 
 /// given an set of assumptions and type variables, return back a substitution set if there
 /// are no conflicting constraints
-pub fn solve_types<T>(constraints: &ConstraintSet<T>) -> Result<SubstitutionSet<T>, String> {
+pub fn solve_types<T>(constraints: &ConstraintSet<T>) -> Result<SubstitutionSet<T>, String>
+where
+    T: Clone + PartialEq + Debug,
+{
     // first, we build out a table referencing type variables to a discrete set of constraints.
     // many type variables can be unified in this step.
     let mut constraints_by_type: ConstraintsByType<T> = ConstraintsByType::new();
-    // we iterate through all constraints. Specifically for the equality constraint, we map type variables.
+    // we iterate through all constraints. Specifically for the equality constraint, we unify constraints
     for c in &constraints.constraints {
         match c {
             Constraint::Equality(ref l, ref r) => {
-            },
-            Constraint::IsLiteral(ref var, ref typ) => {
-                let ref constraint_list = constraints_by_type.get_or_create(var);
-            },
-            Constraint::ImplicitInstanceConstraint(ref lef, ref right) => {
-            },
-            Constraint::IsGeneric(ref lef, ref right) => {
+                constraints_by_type.unify(l, r);
             }
+            Constraint::IsLiteral(ref var, ref typ) => {
+                let constraint_list = constraints_by_type.get_or_create(var);
+                constraint_list.push(Constraint::IsLiteral(var.clone(), typ.clone()))
+            }
+            Constraint::ImplicitInstanceConstraint(ref lef, ref right) => {}
+            Constraint::IsGeneric(ref lef, ref right) => {}
         }
     }
-    panic!();
+    // now we evaluate these constraints.
+    let mut type_by_reference = HashMap::new();
+    let mut substitution_set = SubstitutionSet::new();
+    for type_var in &constraints.types {
+        let typ = solve(&mut constraints_by_type, &mut type_by_reference, &type_var)?;
+        substitution_set.insert(type_var.clone(), typ);
+    }
+    Ok(substitution_set)
+}
+
+/// given constraitns by type, solve the reference specified.
+fn solve<T: Clone + PartialEq + Debug>(
+    constraints_by_type: &mut ConstraintsByType<T>,
+    type_by_reference: &mut HashMap<usize, T>,
+    type_var: &TypeVar,
+) -> Result<Option<T>, String> {
+    let reference = constraints_by_type
+        .reference_by_type
+        .get(type_var)
+        .unwrap_or(&0);
+    // if the calculation has happend already, use it.
+    if let Some(ref typ) = type_by_reference.get(reference) {
+        return Ok(Some((*typ).clone()));
+    }
+
+    let constraints = &constraints_by_type.constraints_by_reference[*reference];
+    let mut typ = None;
+    for c in constraints {
+        match c {
+            Constraint::IsLiteral(ref _var, ref literal_type) => {
+                if typ == None {
+                    typ = Some(literal_type.clone());
+                } else {
+                    if typ != Some((*literal_type).clone()) {
+                        return Err(format!("type mismatch: {:?} and {:?}", typ, literal_type));
+                    }
+                }
+            }
+            // TODO: in the future, we need to resolve generics.
+            _ => {}
+        }
+    }
+    Ok(typ)
 }
 
 pub type ReferenceIndex = usize;
 
-struct ConstraintsByType<T> {
-    constraint_list: Vec<Vec<Constraint<T>>>,
-    constraint_map: HashMap<TypeVar, usize>
+struct ConstraintsByType<T>
+where
+    T: Clone + PartialEq + Debug,
+{
+    pub constraints_by_reference: Vec<Vec<Constraint<T>>>,
+    pub reference_by_type: HashMap<TypeVar, usize>,
 }
 
-impl<T> ConstraintsByType<T> {
+impl<T> ConstraintsByType<T>
+where
+    T: Clone + PartialEq + Debug,
+{
     pub fn new() -> ConstraintsByType<T> {
         ConstraintsByType {
-            constraint_list: vec![],
-            constraint_map: HashMap::new()
+            constraints_by_reference: vec![],
+            reference_by_type: HashMap::new(),
         }
     }
 
-    pub fn get_or_create(&mut self, var: &TypeVar) -> &Vec<Constraint<T>> {
-        let type_index = match self.constraint_map.get(var) {
+    pub fn get_or_create(&mut self, var: &TypeVar) -> &mut Vec<Constraint<T>> {
+        let type_index = match self.reference_by_type.get(var) {
             Some(i) => i.clone(),
             None => {
-                self.constraint_list.push(vec![]);
-                self.constraint_list.len() - 1
+                self.constraints_by_reference.push(vec![]);
+                self.constraints_by_reference.len() - 1
             }
         };
-        return &self.constraint_list[type_index];
+        self.reference_by_type.insert(var.clone(), type_index);
+        return &mut self.constraints_by_reference[type_index];
+    }
+
+    /// given two type vars, unify those values and the
+    /// corresponding constraints.
+    /// this will also remap the constraint indices in
+    /// constraint_map.
+    pub fn unify(&mut self, left: &TypeVar, right: &TypeVar) {
+        // first, we extend the left constraints right the right
+        let right_constraints = self.get_or_create(right).to_owned();
+        self.get_or_create(left).extend(right_constraints);
+        let left_index = match self.reference_by_type.get(left) {
+            Some(i) => i.clone(),
+            None => panic!("should not be able to reach here, as index should always exist after call to get_or_create"),
+        };
+        let right_index = match self.reference_by_type.get(right) {
+            Some(i) => i.clone(),
+            None => panic!("should not be able to reach here, as index should always exist after call to get_or_create"),
+        };
+        // remap right type to the left index.
+        self.reference_by_type.insert(right.clone(), left_index);
+        // delete the reference to the right vector.
+        self.constraints_by_reference[right_index].clear();
     }
 }
 
-
-impl<T> ConstraintSet<T> {
+impl<T> ConstraintSet<T>
+where
+    T: Clone + PartialEq + Debug,
+{
     pub fn new() -> ConstraintSet<T> {
         ConstraintSet {
             constraints: vec![],
@@ -98,10 +179,4 @@ impl<T> ConstraintSet<T> {
 }
 
 /// stores the final result of the type inference algorithm.
-pub struct SubstitutionSet<T>(HashMap<Rc<TypeVar>, Option<T>>);
-
-impl<T> SubstitutionSet<T> {
-    pub fn new() -> SubstitutionSet<T> {
-        SubstitutionSet(HashMap::new())
-    }
-}
+pub type SubstitutionSet<T> = HashMap<Rc<TypeVar>, Option<T>>;
--- inference/src/heeren_tests.rs
+++ inference/src/heeren_tests.rs
@@ -1,31 +1,53 @@
-use super::{AssumptionSet, Constraint,SubstitutionSet};
+use super::{solve_types, Constraint, ConstraintSet};
+use std::rc::Rc;
 
-enum ExamplesTypes {
+#[derive(Clone, PartialEq, Debug)]
+enum ExampleTypes {
     Bool,
-    FunctionType(Vec<Box<ExampleTypes>>, Box<ExamplesTypes>),
-    TypeVariable(Rc<TypeVar>),
+    FunctionType(Vec<Box<ExampleTypes>>, Box<ExampleTypes>),
 }
 
 #[test]
-fn test_example_from_paper() {
-    let mut assumptions = AssumptionSet<ExamplesTypes>::new();
-    let t1 = assumptions.create_type_var();
-    let t2 = assumptions.create_type_var();
-    let t3 = assumptions.create_type_var();
-    let t4 = assumptions.create_type_var();
-    let t5 = assumptions.create_type_var();
-    /// ad constraint that y must consume a bool, and that
-    ///
-    assumptions.constraints.extend(vec![
-        Constraint::IsLiteral(t2.clone(), 
-        ExamplesTypes::FunctionType(
-            vec![ExampleTypes::Bool],
-            ExampleTypes::TypeVariable(t3.clone()),
-        ]),
-        Constraint::ImplicitInstanceConstraint(t4.clone(), t5.clone(), t3.clone()),
-        Constraint::ImplicitInstanceConstraint(t2.clone(), t5.clone(), t1.clone()),
-        Constraint::Equality(t5.clone(), t1.clone()),
-    ]);
-    let substitution = assumptions.solve();
-    assert!(substitution.get);
+fn test_unification() {
+    let mut constraints = ConstraintSet::new();
+    let a = constraints.create_type_var();
+    let b = constraints.create_type_var();
+    let c = constraints.create_type_var();
+    constraints
+        .constraints
+        .push(Constraint::Equality(a.clone(), b.clone()));
+    constraints
+        .constraints
+        .push(Constraint::IsLiteral(a.clone(), ExampleTypes::Bool));
+    let substitution_set = solve_types(&constraints).unwrap();
+    assert!(substitution_set.get(&a) == Some(&Some(ExampleTypes::Bool)));
+    assert!(substitution_set.get(&b) == Some(&Some(ExampleTypes::Bool)));
+    // if there isn't any conflicting information, and the
+    // available variables are insufficient, then type inference
+    // cannot be performed, and the result should be none.
+    assert!(substitution_set.get(&c) == Some(&None));
 }
+
+//#[test]
+//fn test_example_from_paper() {
+//    let mut assumptions = AssumptionSet<ExamplesTypes>::new();
+//    let t1 = assumptions.create_type_var();
+//    let t2 = assumptions.create_type_var();
+//    let t3 = assumptions.create_type_var();
+//    let t4 = assumptions.create_type_var();
+//    let t5 = assumptions.create_type_var();
+//    /// ad constraint that y must consume a bool, and that
+//    ///
+//    assumptions.constraints.extend(vec![
+//        Constraint::IsLiteral(t2.clone(),
+//        ExamplesTypes::FunctionType(
+//            vec![ExampleTypes::Bool],
+//            ExampleTypes::TypeVariable(t3.clone()),
+//        ]),
+//        Constraint::ImplicitInstanceConstraint(t4.clone(), t5.clone(), t3.clone()),
+//        Constraint::ImplicitInstanceConstraint(t2.clone(), t5.clone(), t1.clone()),
+//        Constraint::Equality(t5.clone(), t1.clone()),
+//    ]);
+//    let substitution = assumptions.solve();
+//    assert!(substitution.get);
+//
